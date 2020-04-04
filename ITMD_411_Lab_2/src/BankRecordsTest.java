446366afbbadcfa4160a997ab16eff95dae8a13d
yulambula <42789111+yulambula@users.noreply.github.com>
2020-04-04 18:30:29 -0500

    First Lab 2 commit.

---
+++ ITMD_411_Lab_2/src/BankRecordsTest.java
@@ -0,0 +1,12 @@
+
+public class BankRecordsTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		BankRecords br = new BankRecords();
+		br.readData();
+
+	} // End main()
+
+}
---
+++ ITMD_411_Lab_2/src/Client.java
@@ -0,0 +1,10 @@
+
+public abstract class Client {
+	
+	public abstract void readData(); // Read file detail
+	
+	public abstract void processData(); // Process file detail
+	
+	public abstract void printData(); // Print file detail
+
+} // End class Client
